1a84cf61bbf216b68b06fa988a1eef283db9867d
ddrissa <ddrissaci@gmail.com>
2025-09-10 16:45:29 +0000

    Initialisation des dependances

---
+++ loyers/src/test/java/ci/kossovo/loyers/LoyersApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.kossovo.loyers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
