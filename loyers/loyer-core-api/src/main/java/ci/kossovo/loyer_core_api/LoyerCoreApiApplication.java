ef85cb470416eb962bfe2dc3a36fb2645111ec0e
ddrissa <ddrissaci@gmail.com>
2025-09-17 02:12:26 +0000

    Creation loyers core

---
+++ loyers/loyer-core-api/src/main/java/ci/kossovo/loyer_core_api/LoyerCoreApiApplication.java
@@ -0,0 +1,13 @@
+package ci.kossovo.loyer_core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyerCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyerCoreApiApplication.class, args);
+	}
+
+}
---
+++ loyers/loyer-core-api/src/test/java/ci/kossovo/loyer_core_api/LoyerCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.kossovo.loyer_core_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyerCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
